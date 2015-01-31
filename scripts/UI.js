e0924187e22ac17aa74171a29366967192efc1c0
ettubrutesbro <Jack.Leng@gmail.com>
2015-01-31 01:27:19 -0800

    2d: UI talk > vote transitions set
    also implemented table for data....next is upldown transitions for
    changing between navs

--- scripts/UI.js
+++ scripts/UI.js
@@ -1,10 +1,13 @@
 
 // SESEME UI custom JS - no jQuery
 
+var stickerDrawerOpen = false
 
 var nav = document.getElementById("nav")
 var container = document.querySelector(".container")
 var navs = [].slice.call(nav.children) 
+
+var currentSection
 
 
 
@@ -15,29 +18,26 @@ navs.forEach(function(ele,i,arr){
 		var index = navs.indexOf(ele)
 		var section = document.getElementById(name)
 
+		if(currentSection==section){
+		}
+
 		var others = [].slice.call(document.getElementsByTagName('section'))
 		others.splice(index,1)
 
 		console.log(name + " " + index + " " + section)
-		console.log(others)
+		//console.log(others)
+		console.log('section height is ' + section.style["height"])
+		Velocity(section,'transition.slideUpIn')
+		// if(currentSection!=undefined){
+		// 	Velocity(currentSection,'transition.slideUpDown')
+		// }
+		// section.style["display"] = "block"
+		// // fadeslidedown others, fadeslideup section
+		// others.forEach(function(ele,i,arr){
+		// 	ele.style["display"] = "none"
+		// })
 
-		section.style["display"] = "block"
-		others.forEach(function(ele,i,arr){
-			ele.style["display"] = "none"
-		})
-
-		// 1. get all sections - HTMLCollection --> array
-		// 2. remove section id (name) from that array
-		// 3. .style["display"] = "none"
-
-
-		//fade in test function:
-		// Velocity(document.getElementById(name), 
-		// 	{opacity: 1},
-		// 	{duration: 500})
-
-	console.log(section.offsetHeight)
-	
+		//currentSection = section
 	})
 
 	function stickerDrawer(){
@@ -46,43 +46,65 @@ navs.forEach(function(ele,i,arr){
 
 		categories.forEach(function(ele,i,arr){
 			ele.addEventListener('click',function(){
-				console.log(categories.indexOf(ele)) 
-				//this will eventually be key to pulling the correct sticker drawer out
-				//load in the correct stickers per category, then run the below anim
-				Velocity(target, {height: '2em'},{duration: 500})
-				//this would be even better with a stagger....
+				if(!stickerDrawerOpen){
+					//this will eventually be key to pulling the correct sticker drawer out
+					//load in the correct stickers per category, then run the below anim
+					Velocity(target, {height: '2em'},{duration: 500})
+					//this would be even better with a stagger....
+					stickerDrawerOpen = true
+				}else if(stickerDrawerOpen){
+					Velocity(target, {height: 0}, {duration: 500})
+					stickerDrawerOpen = false
+				}
 			})
 		})
 	} //end stickerdrawer
 
-	function voteDrawer(){
+	function voteConv(){
 		//section conversate display none
-		var button = document.getElementById('switchVote')
-		var otherbutton = document.getElementById('switchConv')
-		var conv = document.getElementById("conversate")
-		var vote = document.getElementById("voteDiv")
+		var section = document.getElementById("talk")
+		var voteButton = document.getElementById('switchVote')
+		var convButton = document.getElementById('switchConv')
+		var conv = document.getElementById("conversate").querySelectorAll(".columns")
+		conv = [].slice.call(conv)
+		var vote = document.getElementById("voteDiv").querySelectorAll("div")
+		vote = [].slice.call(vote)
 
-		button.addEventListener('click',function(){
-			conv.style["display"] = "none"
-			button.style["display"] = "none"
-			otherbutton.style["display"] = "block"
-			vote.style["display"] = "block"
+		voteButton.addEventListener('click',function(){
+			if(stickerDrawerOpen){
+				var target = document.querySelectorAll('#stickerDrawer .row')
+				Velocity(target, {height: 0}, {duration: 500})
+				stickerDrawerOpen = false
+			}
+			Velocity(section, {height: "8em"}, {duration: 700})
+
+			Velocity(voteButton, "transition.slideRightOut", {duration: 200})
+			Velocity(convButton, "transition.slideLeftIn", {delay: 1000})
+			Velocity(conv, "transition.slideRightOut", {duration: 350})
+			setTimeout(function(){
+				Velocity(vote, "transition.slideLeftIn", {stagger: 150},{duration: 500})
+			},350)
 		})
 		
-		otherbutton.addEventListener('click', function(){
-			conv.style["display"] = "block"
-			vote.style["display"] = "none"
-			button.style["display"] = "block"
-			otherbutton.style["display"] = "none"
-		})
+		convButton.addEventListener('click', function(){
+			Velocity(section, {height: "15em"}, {duration: 700})
+			Velocity(voteButton, "transition.slideRightIn", {delay: 1000})
+			Velocity(convButton, "transition.slideLeftOut", {duration: 300})
+			Velocity(vote, "reverse", {duration: 200})
+			setTimeout(function(){
+				vote.forEach(function(ele){
+					ele.style["display"] = 'none'
+				})
+				Velocity(conv, "transition.slideRightIn", {stagger: 10})
+			},350)
+			
 		
+		})	
+	} 
 
-		
+	window.onload = function(){
+		stickerDrawer()
+		voteConv()
 	}
-
-window.onload = function(){
-	stickerDrawer()
-	voteDrawer()
-}
 
 })
